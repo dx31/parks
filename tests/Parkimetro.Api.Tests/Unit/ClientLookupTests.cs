ac0c58fbcbaa11a7cc9e9b1df96f85d71ab67b2e
Dennys Ordoñez <dordonez@Slse.com.pe>
2026-09-16 15:07:09 -0500

    Corregido

--- tests/Parkimetro.Api.Tests/Unit/ClientLookupTests.cs
+++ tests/Parkimetro.Api.Tests/Unit/ClientLookupTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using Parkimetro.Api.Identity;
 
 namespace Parkimetro.Api.Tests.Unit;
--- tests/Parkimetro.Api.Tests/Unit/ParkingStayTests.cs
+++ tests/Parkimetro.Api.Tests/Unit/ParkingStayTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using Parkimetro.Api.Identity;
 using Parkimetro.Api.Models;
 using Parkimetro.Api.Parking;
